6b017317d89d44b0ec01d397351bff8e7f63b2e8
Zeineb136 <104872308+Zeineb136@users.noreply.github.com>
2022-08-07 08:20:42 +0100

    shell.h

--- shell.h
+++ shell.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <dirent.h>
+
+char *standard_input(FILE* fp, size_t size);
+void _ls();
 char *read_standard_input(void);
 void shell_loop(void);
 #endif
